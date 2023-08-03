84a7a04c592aff8a89d99b03d6835ab8dcb0b58c
RISHIRGK <kadamrishi05@gmail.com>
2023-08-03 13:17:38 +0530

    first_commit

---
+++ samaan_frontend/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './input.css';
+
+function App() {
+  return (
+    <div >
+    <div className="  w-[100vw] h-[100vh]   bg-black ">
+      <div className="w-[100%] h-[100%] max-w-screen-2xl mx-auto flex flex-col justify-center items-center ">
+        <div className=" w-[100%] h-[15%] bg-green-400  " ></div>
+        <div className=" w-[100%] h-[85%] bg-red-400 flex flex-col   ">
+          <div id="carousel" className=" w-[100%] h-[80%] bg-pink-400 ">
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
