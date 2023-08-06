c763b91d568d7a00ee34e103d5e399a8e054a2fb
happy1511 <140176910+happy1511@users.noreply.github.com>
2023-08-06 11:28:56 +0530

    Carousel_finished

--- samaan_frontend/src/App.js
+++ samaan_frontend/src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './input.css';
-
+import Header from './Components/Header'
+import Carousel from './Components/Carousel';
 function App() {
   return (
     <div >
     <div className="  w-[100vw] h-[100vh]   bg-black ">
       <div className="w-[100%] h-[100%] max-w-screen-2xl mx-auto flex flex-col justify-center items-center ">
-        <div className=" w-[100%] h-[15%] bg-green-400  " ></div>
+        <div className=" w-[100%]  bg-green-400  " >
+          <Header/>
+        </div>
         <div className=" w-[100%] h-[85%] bg-red-400 flex flex-col   ">
           <div id="carousel" className=" w-[100%] h-[80%] bg-pink-400 ">
-
+            <Carousel/>
           </div>
 
         </div>
