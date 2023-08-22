fdfb68b47008aa912ec514019ab8d06f02641caf
RISHIRGK <kadamrishi05@gmail.com>
2023-08-22 12:09:07 +0530

    product card

--- samaan_frontend/src/App.js
+++ samaan_frontend/src/App.js
@@ -14,23 +14,12 @@ function App() {
   <p>    dfdbkjdbnd kjd zd </p>
     <div className="  w-[100vw] h-[100vh]   ">
       <div className="w-[100%] h-[100%] max-w-screen-2xl mx-auto flex flex-col justify-center items-center ">
-<<<<<<< Updated upstream
         <div className=" w-[100%]  bg-green-400  " >
           <Header/>
         </div>
         <div className=" w-[100%] h-[85%] bg-red-400 flex flex-col   ">
           <div id="carousel" className=" w-[100%] h-[80%] bg-pink-400 ">
             <Carousel/>
-=======
-        <div className=" w-[100%] h-[15%] bg-green-400  " ></div>
-        <div className=" w-[100%] h-[85%] flex flex-col   ">
-          <div id="carousel" className=" w-[100%] h-[80%]  flex flex-wrap justify-center items-center ">
-          <ProductCard/>  
-          <ProductCard/>  
-          <ProductCard/>  
-          <ProductCard/>  
-
->>>>>>> Stashed changes
           </div>
 
         </div>
